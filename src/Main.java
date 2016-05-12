8143f8ce1918372265bebc2f7b90b203ca1adc71
Remzi <remzitdf@abv.bg>
2016-05-12 21:50:26 +0300

    Example03_1

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		Scanner in = new Scanner (System.in);
 		
+		System.out.println("Въведете число:");
+		int a = in.nextInt();
+		
+		if(a<0 && a%2 ==0){
+			System.out.println("Числото е отрицателно и четно.");
+			}else if(a<0 && a%2 ==1){
+				System.out.println("Числото е отрицателно и нечетно.");
+				}else if(a>0 && a%2 ==0){
+					System.out.println("Числото е положително и четно.");
+				}else if(a>0 && a%2 ==1){
+					System.out.println("Числото е положително и нечетно.");
+					}else{
+						System.out.println("Нула.");
+					}
 
 	}
 
